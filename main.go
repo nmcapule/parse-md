87ae44123f1a8b1dfec2aca9f49461905a1c3275
Nathaniel Capule <llss.s.sls.lll@google.com>
2020-02-20 21:12:56 +0800

    Better tokenizer.

--- main.go
+++ main.go
@@ -3,114 +3,65 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
+	"regexp"
 	"strings"
-	"unicode"
 )
 
-type Type int
-
-const (
-	TypeText Type = iota
-	TypeH1
-	TypeH2
-	TypeH3
-	TypeH4
-	TypeBold
-	TypeItalic
-	TypeNewline
-	TypeDivider
+var (
+	wordsRe = regexp.MustCompile(`^[\w \t\.\-]+`)
 )
 
-type Node struct {
-	Value    string
-	Type     Type
-	Children []*Node
-}
-
-func from(token string) *Node {
-	switch strings.TrimSpace(token) {
-	case "":
-		return &Node{token, TypeNewline, nil}
-	case "#":
-		return &Node{token, TypeH1, nil}
-	case "##":
-		return &Node{token, TypeH2, nil}
-	case "###":
-		return &Node{token, TypeH3, nil}
-	case "####":
-		return &Node{token, TypeH4, nil}
-	case "*":
-		return &Node{token, TypeItalic, nil}
-	case "**":
-		return &Node{token, TypeBold, nil}
-	default:
-		return &Node{token, TypeText, nil}
-	}
-}
-
-func split(data []byte, eof bool) (int, []byte, error) {
-	var start int
-
-	// Skip whitespaces.
-	for i := 0; i < len(data); i++ {
-		start = i
-		if data[i] == '\n' || !unicode.IsSpace(rune(data[i])) {
-			break
-		}
+func scopedEqual(left, right []byte) bool {
+	min := len(left)
+	if len(right) < min {
+		min = len(right)
 	}
 
-	for i := start + 1; i < len(data); i++ {
-		if data[i] == data[i-1] {
-			continue
-		}
-		if unicode.IsSpace(rune(data[i])) {
-			return i + 1, data[start:i], nil
-		}
-		if !unicode.IsLetter(rune(data[i])) || (unicode.IsLetter(rune(data[i])) && !unicode.IsLetter(rune(data[i-1]))) {
-			return i, data[start:i], nil
-		}
-	}
-
-	return start, nil, nil
+	return string(left[:min]) == string(right[:min])
 }
 
-func tokenize(reader io.Reader) []string {
-	scanner := bufio.NewScanner(reader)
-	scanner.Split(split)
-
-	var tokens []string
-	for scanner.Scan() {
-		tokens = append(tokens, string(scanner.Bytes()))
-	}
-
-	return tokens
-}
-
-func parse(reader io.Reader) *Node {
-	var stack []*Node
-
-	tokens := tokenize(reader)
-	for _, token := range tokens {
-		node := from(token)
-		if node.Type == TypeText {
-			continue
+func readWhile(data []byte, while byte) int {
+	for i, ch := range data {
+		if ch != while {
+			return i
 		}
-		stack = append(stack, node)
 	}
-
-	return stack[0]
+	return len(data)
 }
 
-// func parse(input string) *Node {
-// 	var buf bytes.Buffer
-// 	// var stack []*Node
-// 	for _, ch := range input {
+func readUntilBytes(data []byte, until []byte) int {
+	for i := range data {
+		if scopedEqual(data[i:], until) {
+			return i + len(until)
+		}
+	}
+	return len(data)
+}
 
-// 	}
+func markdownSplitFunc(data []byte, eof bool) (int, []byte, error) {
+	if eof && len(data) == 0 {
+		return 0, nil, nil
+	}
 
-// 	return nil
-// }
+	if scopedEqual(data, []byte("\n\n")) {
+		return 2, []byte("-newline-"), nil
+	}
+	if scopedEqual(data, []byte("'''")) {
+		advance := readUntilBytes(data[3:], []byte("'''")) + 3
+		return advance, data[:advance], nil
+	}
+	switch data[0] {
+	case '*', '#', '-', '\n', '\t':
+		fallthrough
+	case '[', ']', '(', ')':
+		advance := readWhile(data, data[0])
+		return advance, data[:advance], nil
+	}
+
+	match := wordsRe.Find(data)
+
+	return len(match), match, nil
+}
 
 func main() {
 	const test = `
@@ -121,6 +72,17 @@ func main() {
 
 	And this is an *italic text*.
 
+	And
+
+	'''
+	code block
+	i am
+	'''
+
+	## Ohshit
+
+	I am a [link](http://markdown.com)
+
 	----
 
 	## Hell no
@@ -129,8 +91,13 @@ func main() {
 	multiline.
 	`
 
-	tokens := tokenize(strings.NewReader(test))
-	for _, token := range tokens {
-		fmt.Printf("[%s]\n", token)
+	scanner := bufio.NewScanner(strings.NewReader(test))
+	scanner.Split(markdownSplitFunc)
+	for scanner.Scan() {
+		trimmed := strings.TrimSpace(scanner.Text())
+		if trimmed == "" {
+			continue
+		}
+		fmt.Println("[" + strings.TrimSpace(scanner.Text()) + "]")
 	}
 }
